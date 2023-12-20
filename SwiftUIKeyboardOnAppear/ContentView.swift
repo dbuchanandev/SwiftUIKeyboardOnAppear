ab6fbe5c5532e300771e77463d7789333a1145f8
Donavon Buchanan <5569820+dbuchanandev@users.noreply.github.com>
2023-12-20 14:59:03 -0600

    Initial Commit

---
+++ SwiftUIKeyboardOnAppear/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIKeyboardOnAppear
+//
+//  Created by Donavon Buchanan on 12/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIKeyboardOnAppear/SwiftUIKeyboardOnAppearApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKeyboardOnAppearApp.swift
+//  SwiftUIKeyboardOnAppear
+//
+//  Created by Donavon Buchanan on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKeyboardOnAppearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
